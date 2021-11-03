10df33b89f7839335ff95b8eadb24a159c440067
Simon <simnyb-9@student.ltu.se>
2021-11-03 16:19:45 +0100

    added about us

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -128,10 +128,36 @@ const navigation = {
         // },
       ],
     },
+
+   /*  { id: "about",
+    name: "Om oss",
+    featured: [
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc:
+          "https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg",
+        imageAlt:
+          "Drawstring top with elastic loop closure and textured interior padding.",
+      },
+      {
+        name: "Artwork Tees",
+        href: "#",
+        imageSrc:
+          "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg",
+        imageAlt:
+          "Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.",
+      },
+    ],
+    sections: [
+    ],
+  } */
+
+
   ],
   pages: [
-    { name: "Producenter", href: "#" },
-    { name: "Om oss", href: "#" },
+    { name: "Producenter", href: "../producenter" },
+    { name: "Om oss", href: "../om-oss" },
   ],
 };
 const Navigation = () => {
