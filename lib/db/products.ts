55b446d6cc9aaa1fe0d44fcee17f34e56f2d3cfc
Simon <simnyb-9@student.ltu.se>
2021-11-07 13:21:48 +0100

    browser between categorys should now work!

--- lib/db/products.ts
+++ lib/db/products.ts
@@ -1,5 +1,26 @@
 import client from "./create_client";
 
-export const getProductsAll = () => {};
+export const getProductsAll = async () => {
+    try {
+      const res = await client.query(
+        "SELECT * FROM products;"
+      );
 
-export const getProductsByCategory = (category: string) => {};
+      return res.rows;
+    } catch (err) {
+      console.error(err);
+    }
+    return [];
+  };
+
+export const getProductsByCategory = async (category: string) => {
+  try {
+    const query = 'SELECT * FROM Category c JOIN Products p ON c.id = p.category_id WHERE c.name=$1;';
+    const res = await client.query(query, [category.toLowerCase()]);
+    return res.rows;
+  
+  } catch (err) {
+    console.error(err);
+  }
+  return [];
+};
