d51ff2b2c84ff71520f571e6416bcf96f8c31f7f
norlen <joacim.norlen@gmail.com>
2021-11-08 12:06:05 +0100

    misc fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,15 @@
-import { GetStaticProps, GetStaticPaths } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
+
+import ProductSmall from "@components/products/product_small";
 
 import type { NextPage } from "next";
 import type { Product } from "@lib/types";
 
 import { getProductsAll } from "@lib/db";
-import Link from "next/link";
 
 type StaticProps = {
   products: Product[];
-};
-
-const ProductSmall = ({ product }: { product: any }) => {
-  return (
-    <div className="flex gap-4 flex-col max-w-sm">
-      <Link href={`/produkt/${product.id}`}>
-        <a>
-          <img src={product.image_url} />
-        </a>
-      </Link>
-      <div className="flex flex-col">
-        <p className="text-sm text-gray-200">{product.category}</p>
-        <p className="font-medium">{product.name}</p>
-        <p className="test-gray-800">{product.description}</p>
-      </div>
-    </div>
-  );
 };
 
 const Home: NextPage<StaticProps> = ({ products }) => {
