1bb88cfcf6262d072d2d4b308147185dcfc37998
Sebberydstrom <sebbe.online@gmail.com>
2021-11-10 09:50:55 +0100

    Database updates

--- migrations/sqls/20211109131847-cart-up.sql
+++ migrations/sqls/20211109131847-cart-up.sql
@@ -1,1 +1,9 @@
-/* Replace with your SQL commands */
+CREATE TABLE cart (
+    user_id     INT     NOT NULL,
+    product_id  INT     NOT NULL,
+    quantity    INT     NOT NULL,
+
+    PRIMARY KEY (user_id, product_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
