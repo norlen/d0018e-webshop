ae719f52a1502878a85e301b9972baefc00a104d
Simon <simnyb-9@student.ltu.se>
2021-11-11 13:46:14 +0100

    experimenting with footer

--- components/footer/foot.tsx
+++ components/footer/foot.tsx
@@ -4,16 +4,10 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <>
-      <footer className="footer bg-white relative pt-1 border-b-2 border-blue-700">
-        <div className="container mx-auto px-6">
-          <div className="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
-            <div className="sm:w-2/3 text-center py-6">
-              <p className="text-sm text-green-700 font-bold mb-2">
-                © 2021 by TEAMDATA
-              </p>
-            </div>
-          </div>
-        </div>
+      <footer className="flex flex flex-col items-center border-b border-gray-100">
+        <p className="text-sm text-green-700 font-bold py-4">
+          © 2021 by TEAMDATA
+        </p>
       </footer>
     </>
   );
