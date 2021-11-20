8211450a6cf3140cd788232f8d1e3bead3fb4081
Simonnyberg93 <simonnyberg_93@hotmail.com>
2021-11-21 00:32:44 +0100

    Logout (#53)
    * fixed logout
    
    Co-authored-by: Simon <simnyb-9@student.ltu.se>

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { useUser } from "@lib/hooks";
 
 import Cart from "@components/cart/cart";
+import { fetcher } from "@lib/util";
+import router, { useRouter } from "next/router";
 
 const navigation = {
   pages: [
@@ -36,7 +38,8 @@ const navigation = {
 };
 
 const Navigation = () => {
-  const { user } = useUser();
+  const { user, mutateUser } = useUser();
+  const router = useRouter();
 
   return (
     <nav className="px-8 h-16 flex items-center gap-8 border-b border-gray-100">
@@ -66,14 +69,23 @@ const Navigation = () => {
         {/* Show login and register button if user is not logged in, otherwise logout button. */}
         {user && user.isLoggedIn ? (
           <>
-            <Link href="/logout">
-              <a className="text-sm font-medium text-gray-700 hover:text-green-500">
+            {/* must wrap this in Link? */}
+            <Link href="/">
+              <a
+                className="text-sm font-medium text-gray-700 hover:text-green-500"
+                onClick={async (e) => {
+                  e.preventDefault();
+                  mutateUser(
+                    await fetcher("/api/logout", { method: "POST" }),
+                    false
+                  );
+                  router.push("/login");
+                }}
+              >
                 Logga ut
               </a>
             </Link>
-
             <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
-
             <Cart />
           </>
         ) : (
