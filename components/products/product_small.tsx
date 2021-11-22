8320c14c6e661cd0cd5ecd990e06c8eae6fc6b57
Joacim Norlén <joacim.norlen@gmail.com>
2021-11-22 10:04:24 +0100

    Create order from cart (#54)
    Adds the ability to create an order from cart

--- components/products/product_small.tsx
+++ components/products/product_small.tsx
@@ -28,9 +28,9 @@ const ProductSmall = ({ product }: Props) => {
   // TODO: Global flash for errors.
 
   return (
-    <div className="flex gap-4 flex-col max-w-sm">
+    <div className="flex gap-4 flex-col max-w-sm bg-white rounded-lg shadow-md">
       <Link href={`/produkt/${product.id}`}>
-        <a className="h-96 w-96 relative rounded-lg overflow-hidden">
+        <a className="h-96 w-96 relative rounded-t-lg overflow-hidden">
           <Image
             src={product.image_url}
             layout="fill"
@@ -39,13 +39,13 @@ const ProductSmall = ({ product }: Props) => {
           />
         </a>
       </Link>
-      <div className="flex flex-col">
+      <div className="flex flex-col px-2">
         <p className="text-sm text-gray-500">{product.category}</p>
         <p className="font-medium">{product.name}</p>
         <p className="font-medium">{product.producer}</p>
-        <p className="test-gray-800">{cutoff(product.description, 50)}</p>
+        <p className="test-gray-800">{cutoff(product.description, 45)}</p>
       </div>
-      <div className="flex justify-between align-bottom">
+      <div className="flex justify-between align-bottom px-2 pb-2">
         <Link href={`/produkt/${product.id}`}>
           <a className="hover:text-green-500 pt-1">Läs mer</a>
         </Link>
@@ -53,12 +53,35 @@ const ProductSmall = ({ product }: Props) => {
           <p className="font-medium pt-1">{product.price} kr/kg</p>
           {user && user.isLoggedIn ? (
             <button
-              className={`py-2 px-4 rounded-md shadow-md bg-green-300 hover:bg-green-500 ${
+              className={`py-2 px-4 rounded-md shadow-md bg-green-300 hover:bg-green-500 w-24 h-10 ${
                 loading ? "disabled opacity-50" : ""
               }`}
               onClick={() => addToCart(product.id, 1)}
             >
-              Köp 1 kg
+              {loading ? (
+                <svg
+                  className={`animate-spin h-5 w-5 mx-auto text-white`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              ) : (
+                <>Köp 1 kg</>
+              )}
             </button>
           ) : (
             <button className="py-2 px-4 rounded-md shadow-md bg-green-300 hover:bg-green-500">
---
+++ lib/db/order.ts
@@ -0,0 +1,193 @@
+import create_client from "./create_client";
+
+export type Order = {
+  id: string;
+  name: string;
+  phonenumber: string;
+  email: string;
+  address: string;
+  items: OrderItem[];
+  userid: string;
+  status: number;
+};
+
+export type OrderItem = {
+  name: string;
+  producer: string;
+  category: string;
+  price: number;
+  quantity: number;
+  imageurl: string;
+};
+
+export const getOrderById = async (id: string): Promise<Order> => {
+  const sql = `
+  SELECT id,
+         order_status AS orderstatus,
+         name,
+         phone_number AS phonenumber,
+         email,
+         address,
+         user_id AS userid,
+         order_status AS status
+  FROM orders
+  WHERE id = $1;
+  `;
+
+  const client = create_client();
+  let order = undefined;
+  try {
+    await client.connect();
+    const res = await client.query(sql, [id]);
+    if (res.rows.length > 0) {
+      order = res.rows[0];
+      order.items = [];
+    }
+  } catch (err) {
+    console.error("ERROR getOrderById:", err);
+  } finally {
+    await client.end();
+  }
+  return order;
+};
+
+export const getOrderItems = async (orderId: string): Promise<OrderItem[]> => {
+  const sql = `
+  SELECT p.name,
+         c.name AS category,
+         pr.name AS producer,
+         o.price,
+         o.quantity,
+         p.image_url AS imageUrl
+  FROM order_products AS o
+    INNER JOIN products AS p ON p.id = o.product_id
+    INNER JOIN category AS c ON c.id = p.category_id
+    INNER JOIN producer AS pr ON pr.id = p.producer_id
+  WHERE o.order_id = $1;
+  `;
+
+  const client = create_client();
+  let items = [];
+  try {
+    await client.connect();
+    const res = await client.query(sql, [orderId]);
+    items = res.rows;
+  } catch (err) {
+    console.error("ERROR getitemsById:", err);
+  } finally {
+    await client.end();
+  }
+  return items;
+};
+
+export type CustomerInfo = {
+  id: string;
+  name: string;
+  phoneNumber: string;
+  email: string;
+  address: string;
+};
+
+export type CreateOrderItem = {
+  id: string;
+  quantity: number;
+};
+
+export const createOrder = async (
+  customer: CustomerInfo,
+  items: CreateOrderItem[],
+  subtotal: number
+): Promise<string | undefined> => {
+  const createOrderSql = `
+  INSERT INTO orders (
+    user_id,
+    order_status,
+    name,
+    phone_number,
+    email,
+    address
+  )
+  VALUES
+    ($1, $2, $3, $4, $5, $6)
+  RETURNING id;
+  `;
+
+  const createOrderItemSql = `
+  INSERT INTO order_products (
+    order_id,
+    product_id,
+    price,
+    quantity
+  )
+  SELECT $1 as order_id,
+         p.id as product_id,
+         p.price as price,
+         $3 as quantity
+  FROM products AS p
+  WHERE
+    p.id = $2
+  RETURNING (price * $3) AS price;
+  `;
+
+  const removeCartItemsSql = `
+  DELETE FROM cart
+  WHERE
+    user_id = $1;
+  `;
+
+  let orderId = undefined;
+  const client = create_client();
+  try {
+    await client.connect();
+
+    // To ensure a consistent state, we have to make sure these are all done inside a transaction.
+    //
+    // There's quite a few RTTs here, we could remedy all this by creating a function in the database
+    // which does all of this for us, but let's keep this sane, this is perfectly fine for our use case.
+    await client.query("BEGIN");
+
+    // Create actual order.
+    const createOrderResponse = await client.query(createOrderSql, [
+      customer.id,
+      0,
+      customer.name,
+      customer.phoneNumber,
+      customer.email,
+      customer.address,
+    ]);
+    if (createOrderResponse.rows.length > 0) {
+      orderId = createOrderResponse.rows[0].id;
+    }
+
+    // Add all the products contained in the order.
+    // This can probably be done in a single query, but let's keep it simpler.
+    let total = 0;
+    for (let item of items) {
+      const res = await client.query(createOrderItemSql, [
+        orderId,
+        item.id,
+        item.quantity,
+      ]);
+      if (res.rows.length > 0) {
+        total += parseInt(res.rows[0].price);
+      }
+    }
+    if (subtotal != total) {
+      // Price mismatch from what we want to charge the customer, compared to what they saw in the client.
+      throw Error("price mismatch");
+    }
+
+    // Remove these products from the cart. Let's clear the entire cart removing items that
+    // are not necessarily in the order. An alternative could be to only remove items we put
+    // in the order. But let's keep a clean slate for the customer after an order.
+    await client.query(removeCartItemsSql, [customer.id]);
+
+    await client.query("COMMIT");
+  } catch (err) {
+    await client.query("ROLLBACK");
+    console.error(err);
+  } finally {
+    await client.end();
+  }
+  return orderId;
+};
