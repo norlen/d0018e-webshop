cc2bdc81cff9f46925c2eaa231c4c6f3db2d6234
norlen <joacim.norlen@gmail.com>
2021-11-22 10:00:19 +0100

    fixes

--- components/products/product.tsx
+++ components/products/product.tsx
@@ -45,12 +45,35 @@ const ProductC = ({ product }: Props) => {
           </p>
           {user && user.isLoggedIn ? (
             <button
-              className={`py-2 px-4 rounded-md shadow-md bg-green-300 hover:bg-green-500 ${
+              className={`py-2 px-4 rounded-md shadow-md bg-green-300 hover:bg-green-500 w-24 h-10 ${
                 loading ? "disabled opacity-50" : ""
               }`}
               onClick={() => addToCart(product.id, 1)}
             >
-              Köp 1 kg
+              {loading ? (
+                <svg
+                  className={`animate-spin h-5 w-5 mx-auto text-white`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              ) : (
+                <>Köp 1 kg</>
+              )}
             </button>
           ) : (
             <button className="py-2 px-4 rounded-md shadow-md bg-green-300 hover:bg-green-500">
--- components/products/product_small.tsx
+++ components/products/product_small.tsx
@@ -43,7 +43,7 @@ const ProductSmall = ({ product }: Props) => {
         <p className="text-sm text-gray-500">{product.category}</p>
         <p className="font-medium">{product.name}</p>
         <p className="font-medium">{product.producer}</p>
-        <p className="test-gray-800">{cutoff(product.description, 50)}</p>
+        <p className="test-gray-800">{cutoff(product.description, 45)}</p>
       </div>
       <div className="flex justify-between align-bottom px-2 pb-2">
         <Link href={`/produkt/${product.id}`}>
@@ -53,12 +53,35 @@ const ProductSmall = ({ product }: Props) => {
           <p className="font-medium pt-1">{product.price} kr/kg</p>
           {user && user.isLoggedIn ? (
             <button
-              className={`py-2 px-4 rounded-md shadow-md bg-green-300 hover:bg-green-500 ${
+              className={`py-2 px-4 rounded-md shadow-md bg-green-300 hover:bg-green-500 w-24 h-10 ${
                 loading ? "disabled opacity-50" : ""
               }`}
               onClick={() => addToCart(product.id, 1)}
             >
-              Köp 1 kg
+              {loading ? (
+                <svg
+                  className={`animate-spin h-5 w-5 mx-auto text-white`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              ) : (
+                <>Köp 1 kg</>
+              )}
             </button>
           ) : (
             <button className="py-2 px-4 rounded-md shadow-md bg-green-300 hover:bg-green-500">
