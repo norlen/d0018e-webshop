bd9abfe95d6686a0641bd3c55e3cf3885d55691c
Simon <simnyb-9@student.ltu.se>
2021-11-27 10:38:39 +0100

    fixed admin add to cart issue, and also split buy button to seperate component

--- components/products/product.tsx
+++ components/products/product.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
-import { useAddToCart, useUser } from "@lib/hooks";
+import BuyButton from "./buyButton";
 
 type Props = {
   product: {
@@ -15,8 +14,6 @@ type Props = {
 };
 
 const ProductC = ({ product }: Props) => {
-  const { loading, error, addToCart } = useAddToCart();
-  const { user } = useUser();
   // TODO: Global flash for errors.
 
   return (
@@ -43,45 +40,7 @@ const ProductC = ({ product }: Props) => {
           <p className="">
             Pris: <span className="font-medium">{product.price} kr/kg</span>
           </p>
-          {user && user.isLoggedIn ? (
-            <button
-              className={`py-2 px-4 rounded-md shadow-md bg-green-300 hover:bg-green-500 w-24 h-10 ${
-                loading ? "disabled opacity-50" : ""
-              }`}
-              onClick={() => addToCart(product.id, 1)}
-            >
-              {loading ? (
-                <svg
-                  className={`animate-spin h-5 w-5 mx-auto text-white`}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
-              ) : (
-                <>Köp 1 kg</>
-              )}
-            </button>
-          ) : (
-            <button className="py-2 px-4 rounded-md shadow-md bg-green-300 hover:bg-green-500">
-              <Link href="/login">
-                <a>Köp 1 kg</a>
-              </Link>
-            </button>
-          )}
+          <BuyButton productID={product.id} />
         </div>
       </div>
     </div>
