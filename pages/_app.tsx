4ecf6088cbd8c487642a841a4fd2ecb1782be4c7
norlen <joacim.norlen@gmail.com>
2021-12-05 14:17:54 +0100

    adds a toast when adding products to cart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@components/layout/store";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <div>
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+          gutter={8}
+          toastOptions={{
+            custom: {
+              duration: 1000,
+            },
+          }}
+        />
+      </div>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
