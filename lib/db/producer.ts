cb86aeecfabba210d3582263a24eb3f18daf20e2
Simon <simnyb-9@student.ltu.se>
2021-12-07 13:47:42 +0100

    Updated DB-data and added description on producers

--- lib/db/producer.ts
+++ lib/db/producer.ts
@@ -4,22 +4,29 @@ export type Producer = {
   id: string;
   name: string;
   image_url: string;
+  description: string;
 };
 
 export const getProducerById = async (
   id: string
 ): Promise<Producer | undefined> => {
   const sql = `
-  SELECT *
-  FROM Producer
-  WHERE id=$1;`;
+  SELECT p.id,
+         p.name,
+         p.image_url,
+         p.description
+  FROM Producer AS p
+  WHERE p.id=$1;`;
   return await getSingleRow(sql, [id]);
 };
 
 export const getProducersAll = async () => {
   const sql = `
-    SELECT *
-    FROM Producer;
+    SELECT p.id,
+           p.name,
+           p.image_url,
+          p.description
+    FROM Producer AS p;
     `;
   return await getMultipleRows(sql);
 };
